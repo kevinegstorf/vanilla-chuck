ff9a45acd2fe6135b4660b141e2b1576f8840449
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2020-01-13 16:02:42 +0100

    added code to render data from api

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const jokesButton = document.getElementById("jokes");
+
+function logError(error) {
+  console.log("Looks like there was a problem:", error);
+}
+
+function getJokes(data) {
+  return data.json();
+}
+
+function makeJokesCollection(data) {
+  let item = "";
+  let jokes = [];
+
+  data.value.map(joke => {
+    item += joke.joke;
+    jokes.push(item);
+  });
+  return jokes;
+}
+
+function renderJokes(jokes) {
+  const ul = document.querySelector(".jokes-contianer");
+
+  jokes.map(joke => {
+    let li = document.createElement("li");
+    li.innerText = joke;
+    ul.appendChild(li);
+  });
+}
+
+function FetchChuckJokes() {
+  fetch("http://api.icndb.com/jokes/random/10.")
+    .then(getJokes)
+    .then(makeJokesCollection)
+    .then(foo)
+    .catch(logError);
+}
+
+jokesButton.addEventListener("click", () => {
+  FetchChuckJokes();
+});
