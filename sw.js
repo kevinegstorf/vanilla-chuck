a99bd70d1aea494eff47c6129000d79aebfb6506
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2020-01-17 10:35:02 +0100

    bumpt sw version

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ const filesToCache = [
   "pages/offline.html"
 ];
 
-const staticCacheName = "vanilla-chuck-pages-cache-v2";
+const staticCacheName = "vanilla-chuck-pages-cache-v3";
 
 self.addEventListener("install", event => {
   // console.log("Attempting to install service worker and cache static assets");
